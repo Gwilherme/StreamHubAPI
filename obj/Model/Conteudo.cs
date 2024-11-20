575039dc20ef3e9f89e21288a252cd0f6ba1be83
Gabriel Antonio <gabrielantonioalmeidasilva@gmail.com>
2024-11-20 19:19:08 -0300

    commit2

---
+++ obj/Model/Conteudo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Conteudo
+{
+	public Conteudo()
+	{
+		public int Id { get; set; }
+		public string nomeconteudo { get; set; }
+	public string comentarios { get; set; }
+	public int like { get; set; }
+	}
+}
---
+++ obj/Model/playlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Playlist
+{
+	public playlist()
+	{
+		public int Id { get;set;}
+	public string nome { get;set;}
+	public Usuarios usuarios { get; set; }
+
+
+
+	}
+}
